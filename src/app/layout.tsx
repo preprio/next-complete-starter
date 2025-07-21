cb6ff44601ed312520c24242de4a284e09d218ed
Mandy Lingham <mandy.lingham@prepr.io>
2025-07-21 14:03:44 +0200

    remove access token from project for public release

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,11 +32,7 @@ export default async function RootLayout({children,}: {children: React.ReactNode
         <Script
           id={'prepr_script'}
           dangerouslySetInnerHTML={{
-            __html: `! function (e, t, p, r, n, a, s) {
-      e[r] || ((n = e[r] = function () {
-      n.process ? n.process.apply(n, arguments) : n.queue.push(arguments)
-      }).queue = [], n.t = +new Date, (a = t.createElement(p)).async = 1, a.src = "https://cdn.tracking.prepr.io/js/prepr_v2.min.js?t=" + 864e5 * Math.ceil(new Date / 864e5), (s = t.getElementsByTagName(p)[0]).parentNode.insertBefore(a, s))
-      }(window, document, "script", "prepr"), prepr("init", "ac_02b4a145e69be5488eb0e3abeada8ae5a2f88b0af09b33d442533b59e3bf4cd8"), prepr("event", "pageload");`,
+            __html: `YOUR-PREPR-TRACKING-CODE`,
           }}></Script>
       </head>
       <body className={ubuntu.className}>
